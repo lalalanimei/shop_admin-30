ce4427a687cafce7054ddc72f0fb4b2870cdc2c1
lalalanimei <1841568377@qq.com>
2018-12-24 17:20:16 +0800

    环境搭建完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>这是login组件</div>
+  <div id="login">
+    <div class="login-box"></div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less'>
+#login {
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .login-box {
+    width: 400px;
+    height: 300px;
+    background-color: #fff;
+    margin: 200px auto;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 导入element-ui
+import ElementUi from 'element-ui'
+// 导入element-ui 的样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入通用样式
+import '@/assets/base.css'
+// 使用element-ui插件
+Vue.use(ElementUi)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
